02944ce22eafcdf0ef6fa7ad53c23efcce6da702
tFury <thomas_haenig@gmx.de>
2018-08-24 21:04:11 +0200

    update test

--- test/index-test.ts
+++ test/index-test.ts
@@ -5,12 +5,36 @@ import * as chaiAsPromised from "chai-as-promised";
 import { ELoglevel, ETransportType } from "../src/utils/enums";
 //#endregion
 
-describe("checks the Logger for no input", () => {
+describe("checks the Logger with no additional input while declaration", () => {
     let logger = new Logger();
 
-    describe("test logger.info", () => {
+    describe("logger.info(\"test\")", () => {
         it("should return \"INFO - test\"", (done) => {
             chai.expect(logger.info("test")).to.equal("INFO - test");
+            done();
+        });
+    });
+    describe("logger.debug(\"test\")", () => {
+        it("should return \"DEBUG - test\"", (done) => {
+            chai.expect(logger.debug("test")).to.equal("DEBUG - test");
+            done();
+        });
+    });
+    describe("logger.error(\"test\")", () => {
+        it("should return \"ERROR - test\"", (done) => {
+            chai.expect(logger.error("test")).to.equal("ERROR - test");
+            done();
+        });
+    });
+    describe("logger.trace(\"test\")", () => {
+        it("should return \"TRACE - test\"", (done) => {
+            chai.expect(logger.trace("test")).to.equal("TRACE - test");
+            done();
+        });
+    });
+    describe("logger.warn(\"test\")", () => {
+        it("should return \"WARN - test\"", (done) => {
+            chai.expect(logger.warn("test")).to.equal("WARN - test");
             done();
         });
     });
